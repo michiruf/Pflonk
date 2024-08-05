3cd3d697e143814b82547f1f1844a712005ae7e3
Michael Ruf <git@michiruf.de>
2024-08-05 20:33:15 +0200

    Add data to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,35 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+    use WithoutModelEvents;
+
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->seedMandatoryData();
 
+        if (app()->isLocal()) {
+            $this->seedLocalData();
+        }
+    }
+
+    private function seedMandatoryData(): void
+    {
+    }
+
+    private function seedLocalData(): void
+    {
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Product::factory(10)->create();
     }
 }
