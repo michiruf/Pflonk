6cd8d94e08d20fe27f0ad70d0c3ba8b612572b0b
Michael Ruf <git@michiruf.de>
2024-08-05 15:21:03 +0200

    Add schedule for heath and horizon

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Spatie\Health\Commands\DispatchQueueCheckJobsCommand;
+use Spatie\Health\Commands\RunHealthChecksCommand;
+use Spatie\Health\Commands\ScheduleCheckHeartbeatCommand;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Horizon
+Schedule::command('horizon:snapshot')->everyFiveMinutes()->sentryMonitor();
+
+// Health
+Schedule::command(ScheduleCheckHeartbeatCommand::class)->everyMinute()->sentryMonitor();
+Schedule::command(DispatchQueueCheckJobsCommand::class)->everyMinute()->sentryMonitor();
+Schedule::command(RunHealthChecksCommand::class)->everyMinute()->sentryMonitor();
