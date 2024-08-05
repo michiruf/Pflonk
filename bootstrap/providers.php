37d218eafc3ff3e6b09db1b619a851d37417ce1a
Michael Ruf <git@michiruf.de>
2024-08-05 16:02:28 +0200

    Install filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HealthServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\PulseAuthorizationServiceProvider::class,
