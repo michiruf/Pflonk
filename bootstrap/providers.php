823850c9e70a0065b1b7afbe0ba451897180c820
Michael Ruf <git@michiruf.de>
2024-08-05 15:18:47 +0200

    Configure pulse and horizon access

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\HealthServiceProvider::class,
+    App\Providers\PulseAuthorizationServiceProvider::class,
 ];
