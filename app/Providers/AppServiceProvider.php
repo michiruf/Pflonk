61c0d34abf72585af25a83f58d18837586d77fb8
Michael Ruf <git@michiruf.de>
2024-08-08 20:25:34 +0200

    URLs must use https on production or staging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,23 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Force usage of https when loading vite resources on a production or staging server
+        $isProduction = config('app.env') === 'production';
+        $isStaging = config('app.env') === 'staging' || str(config('app.url'))->contains('staging');
+        if ($isProduction || $isStaging) {
+            URL::forceScheme('https');
+        }
     }
 }
