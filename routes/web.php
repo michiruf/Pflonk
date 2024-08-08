ce6713b1c05c9bfb0e79973cc3e3d4486463fff6
Michael Ruf <git@michiruf.de>
2024-08-08 20:01:35 +0200

    Add health check and filament endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Health\Http\Controllers\HealthCheckJsonResultsController;
+use Spatie\Health\Http\Controllers\HealthCheckResultsController;
 
 Route::get('/', fn () => view('index'));
 Route::get('/contact', fn () => view('contact'));
@@ -9,3 +11,10 @@ Route::get('/contact', fn () => view('contact'));
 Route::get('/cookie-policy', fn () => view('static.cookie-policy'));
 Route::get('/privacy-policy', fn () => view('static.privacy-policy'));
 Route::get('/terms-of-use', fn () => view('static.terms-of-use'));
+
+// Filament
+Route::redirect('/login', '/admin/login')->name('login'); // filament needs a 'login' route after logout
+
+// Health
+Route::get('health', HealthCheckResultsController::class);
+Route::get('health.json', HealthCheckJsonResultsController::class);
